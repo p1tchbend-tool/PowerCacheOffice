0f85574598ce9359afeea7cd7e2dabdbf14dce6b
p1tchbend-tool <p1tchbend.tool@gmail.com>
2024-02-04 21:06:05 +0900

    最近開いたファイル機能用フォーム

---
+++ Form3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace PowerCacheOffice
+{
+    public partial class Form3 : Form
+    {
+        public string SelectedFile {  get; set; }
+
+        public Form3(List<string> recentFiles)
+        {
+            SelectedFile = string.Empty;
+
+            InitializeComponent();
+
+            listBox1.BeginUpdate();
+            listBox1.Items.Clear();
+            foreach (var item in recentFiles.AsEnumerable().Reverse())
+            {
+                listBox1.Items.Add(item);
+            }
+            listBox1.EndUpdate();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            listBox1.Click += (s, eventArgs) =>
+            {
+                if (listBox1.SelectedItem != null)
+                {
+                    SelectedFile = listBox1.SelectedItem.ToString();
+                    this.Close();
+                }
+            };
+        }
+    }
+}

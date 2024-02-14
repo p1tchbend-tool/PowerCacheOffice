30139ae11838f7b220fff2ffce8b75c4d1be0d7f
p1tchbend-tool <p1tchbend.tool@gmail.com>
2024-02-14 22:39:46 +0900

    不具合の修正

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace PowerCacheOffice
     internal static class Program
     {
         public static readonly string AppName = "Power Cache Office";
-        public static readonly string AppVersion = "1.3.0";
+        public static readonly string AppVersion = "1.3.1";
 
         private static readonly string powerCacheOfficeLogFolder = Path.Combine(
             Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"PowerCacheOffice\log");
